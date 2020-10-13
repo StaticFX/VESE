78fa2c749780028cf69c1f5c69bbbb43d326d393
Devin <devinfritz@gmx.de>
2020-10-13 17:51:45 +0200

    Init commit

---
+++ src/main/java/de/vese/vese/VESE.java
@@ -0,0 +1,21 @@
+package de.vese.vese;
+
+
+
+/*
+    VESE - VESE
+    Version: 1.0.0
+    Author: Devin, David
+    Alias: StaticRed, Dawitschi
+ */
+
+/*
+
+
+
+    
+ */
+
+
+public class VESE {
+}
